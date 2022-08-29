8a13d1cd5c0795447d51ee99714ca4535fbecfbd
ArpitVadariya <arpit092vadariya@gmail.com>
2022-08-29 21:29:59 +0530

    Tuple added

---
+++ Python/Tuple49.py
@@ -0,0 +1,22 @@
+#  This is Tuple Program
+
+
+print("\n to store data in tuple add values with comma saperated between two parentheses bracket")
+print("\n example :- ")
+print("\n (1, 2, 3, 4, 5) or () or (1,)")
+print("\n\n")
+
+tuple1 = (1, 2, 3, 4, 5)
+tuple2 = ("ABC", "DEF")
+
+print("\n tuple1")
+print(tuple1)
+print("\n tuple2")
+print(tuple2)
+
+print("\n tuple3 = tuple1 + tuple2")
+tuple3 = tuple1 + tuple2
+print(tuple3)
+
+print("\n to find tuple length use len(tuple3)")
+print(len(tuple3))
