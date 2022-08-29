3a8069b191311f4957f67936014e2a5508ec8d38
ArpitVadariya <arpit092vadariya@gmail.com>
2022-08-29 21:30:32 +0530

    Set added

---
+++ Python/Set50.py
@@ -0,0 +1,20 @@
+# This is set program
+
+print("\n to store data in set add values with comma saperated between two curly bracket")
+print("\n example :- ")
+print("\n {1, 2, 3, 4, 5}")
+print("\n\n")
+
+set1 = {1, 2, 3, 4, 5}
+print(set1)
+
+print("\n to add item in set use set1.add(6)")
+set1.add(6)
+print(set1)
+
+print("\n to remove item from set use set1.discard(1)")
+set1.discard(1)
+print(set1)
+
+print("\n to find length of set use len(set1)")
+print(len(set1))
